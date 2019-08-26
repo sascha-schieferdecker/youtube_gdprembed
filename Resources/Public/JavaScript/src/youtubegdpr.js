c2992517c331c4e93016fd11c720b8844decf5f8
sascha_schieferdecker <sascha@sascha-schieferdecker.de>
2019-08-26 16:16:52 +0200

    Settings Hiderel works

--- Resources/Public/JavaScript/src/youtubegdpr.js
+++ Resources/Public/JavaScript/src/youtubegdpr.js
@@ -1,42 +1,60 @@
-/*import "core-js"
-import "regenerator-runtime"*/
+import "core-js"
+import "regenerator-runtime"
 
 /**
  * GDPR compliant youtube embedding
  * @param {string} contentId
- * @param {string} ratio
+ * @param {string} hideRel
  * @param {boolean} setCookie
- * @param {sting} videoId
  */
-export default function youtubegdpr(contentId, setCookie) {
+export default function youtubegdpr(contentId, hideRel, setCookie) {
 
-        document.querySelectorAll('.acceptgdpr').forEach(elem => {
-            elem.style.display = 'none';
-        })
+    document.querySelectorAll('.acceptgdpr').forEach(elem => {
+        elem.style.display = 'none';
+    })
 
-        if (window.youtubegdprExecuted != true) {
-            var tag = document.createElement('script');
+    if (window.youtubegdprExecuted != true) {
+        var tag = document.createElement('script');
 
-            tag.src = "https://www.youtube.com/iframe_api";
-            var firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-            window.onYouTubeIframeAPIReady = function() {
-                var playerDivs = document.querySelectorAll('.gdprplayer')
-                var playerDivsArr = [].slice.call(playerDivs); // nodelist to array to use forEach();
-                var players = new Array(playerDivsArr.length);
-                playerDivsArr.forEach(function (e, i) { // forEach ...
-                    players[i] = new YT.Player(e.id, {
-                        videoId: e.getAttribute('data-video'),
-                        events: {
-                            'onStateChange': onPlayerStateChange
-                        }
-                    })
+        window.onYouTubeIframeAPIReady = function() {
+            var playerDivs = document.querySelectorAll('.gdprplayer')
+            var playerDivsArr = [].slice.call(playerDivs); // nodelist to array to use forEach();
+            var players = new Array(playerDivsArr.length);
+            playerDivsArr.forEach(function (e, i) { // forEach ...
+                players[i] = new YT.Player(e.id, {
+                    videoId: e.getAttribute('data-video'),
+                    events: {
+                        'onStateChange': onPlayerStateChange
+                    }
                 })
-            }
+                if (Number(hideRel) > 0) {
+                    let elem = document.getElementById(e.id);
+                    elem.parentNode.parentNode.addEventListener("click", function() {
+                        let playerState = players[i].getPlayerState();
+                        if (playerState == YT.PlayerState.ENDED) {
+                            players[i].seekTo(0);
+                        } else if (playerState == YT.PlayerState.PAUSED) {
+                            players[i].playVideo();
+                        }
+                    });
+                }
+            })
         }
+    }
+
+    function playVideo(target) {
+        target.playVideo();
+    }
+    function rewindVideo(target) {
+        target.seekTo(0);
+    }
 
     let onPlayerStateChange = function(event) {
+        if (Number(hideRel) > 0) {
             if (event.data == YT.PlayerState.ENDED) {
                 event.target.a.parentNode.classList.add("ended");
             } else if (event.data == YT.PlayerState.PAUSED) {
@@ -45,10 +63,11 @@ export default function youtubegdpr(contentId, setCookie) {
                 event.target.a.parentNode.classList.remove("ended");
                 event.target.a.parentNode.classList.remove("paused");
             }
+        }
     };
 
-        window.youtubegdprExecuted = true;
-    }
+    window.youtubegdprExecuted = true;
+}
 
 
 
