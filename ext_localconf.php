f51d720655cb42d0265b0d7d58a60631943bd0ae
sascha_schieferdecker <sascha@sascha-schieferdecker.de>
2023-05-04 11:43:38 +0200

    Reintegrate code from master

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,8 +5,8 @@ if (!defined('TYPO3_MODE')) {
 $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
 $iconRegistry->registerIcon(
     'tx-youtube-gdprembed-video',
-    \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-    ['source' => 'EXT:youtube_gdprembed/ext_icon.svg']
+    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    ['source' => 'EXT:youtube_gdprembed/Resources/Public/Images/Icons/ext_icon_content.svg']
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:youtube_gdprembed/Configuration/TSconfig/ContentElementWizard.txt">');
